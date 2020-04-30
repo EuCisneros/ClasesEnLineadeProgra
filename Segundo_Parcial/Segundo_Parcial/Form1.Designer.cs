0cdb937614ff7fe314c5771f4a3ddbd7b67ae435
Hyung97 <tammyhyunglee@gmail.com>
2020-04-30 14:09:31 -0600

    Proceso del parcial

---
+++ Segundo_Parcial/Segundo_Parcial/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Segundo_Parcial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PanelBarraTitulo = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnProfesores = new System.Windows.Forms.Button();
+            this.btnStudents = new System.Windows.Forms.Button();
+            this.btnMatricula = new System.Windows.Forms.Button();
+            this.PanelContenedorPrincipal = new System.Windows.Forms.Panel();
+            this.PanelBarraTitulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelBarraTitulo
+            // 
+            this.PanelBarraTitulo.BackColor = System.Drawing.Color.Firebrick;
+            this.PanelBarraTitulo.Controls.Add(this.pictureBox1);
+            this.PanelBarraTitulo.Controls.Add(this.label1);
+            this.PanelBarraTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelBarraTitulo.Location = new System.Drawing.Point(0, 0);
+            this.PanelBarraTitulo.Name = "PanelBarraTitulo";
+            this.PanelBarraTitulo.Size = new System.Drawing.Size(969, 62);
+            this.PanelBarraTitulo.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(73, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(533, 56);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Alquiler de Películas";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(11, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(56, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.Goldenrod;
+            this.panelMenu.Controls.Add(this.label2);
+            this.panelMenu.Controls.Add(this.btnProfesores);
+            this.panelMenu.Controls.Add(this.btnStudents);
+            this.panelMenu.Controls.Add(this.btnMatricula);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 62);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(170, 406);
+            this.panelMenu.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(22, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 31);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Alquiler";
+            // 
+            // btnProfesores
+            // 
+            this.btnProfesores.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnProfesores.FlatAppearance.BorderSize = 0;
+            this.btnProfesores.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(76)))));
+            this.btnProfesores.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(45)))), ((int)(((byte)(53)))));
+            this.btnProfesores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProfesores.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProfesores.ForeColor = System.Drawing.Color.Black;
+            this.btnProfesores.Image = ((System.Drawing.Image)(resources.GetObject("btnProfesores.Image")));
+            this.btnProfesores.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnProfesores.Location = new System.Drawing.Point(3, 143);
+            this.btnProfesores.Name = "btnProfesores";
+            this.btnProfesores.Size = new System.Drawing.Size(176, 42);
+            this.btnProfesores.TabIndex = 19;
+            this.btnProfesores.Text = "  Alquiler";
+            this.btnProfesores.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.btnProfesores.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnProfesores.UseVisualStyleBackColor = true;
+            // 
+            // btnStudents
+            // 
+            this.btnStudents.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnStudents.FlatAppearance.BorderSize = 0;
+            this.btnStudents.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(76)))));
+            this.btnStudents.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(45)))), ((int)(((byte)(53)))));
+            this.btnStudents.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStudents.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStudents.ForeColor = System.Drawing.Color.Black;
+            this.btnStudents.Image = ((System.Drawing.Image)(resources.GetObject("btnStudents.Image")));
+            this.btnStudents.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnStudents.Location = new System.Drawing.Point(3, 95);
+            this.btnStudents.Name = "btnStudents";
+            this.btnStudents.Size = new System.Drawing.Size(176, 42);
+            this.btnStudents.TabIndex = 17;
+            this.btnStudents.Text = "  Películas";
+            this.btnStudents.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.btnStudents.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnStudents.UseVisualStyleBackColor = true;
+            // 
+            // btnMatricula
+            // 
+            this.btnMatricula.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnMatricula.FlatAppearance.BorderSize = 0;
+            this.btnMatricula.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(69)))), ((int)(((byte)(76)))));
+            this.btnMatricula.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(45)))), ((int)(((byte)(53)))));
+            this.btnMatricula.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMatricula.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMatricula.ForeColor = System.Drawing.Color.Black;
+            this.btnMatricula.Image = ((System.Drawing.Image)(resources.GetObject("btnMatricula.Image")));
+            this.btnMatricula.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnMatricula.Location = new System.Drawing.Point(3, 47);
+            this.btnMatricula.Name = "btnMatricula";
+            this.btnMatricula.Size = new System.Drawing.Size(176, 42);
+            this.btnMatricula.TabIndex = 15;
+            this.btnMatricula.Text = "  Clientes";
+            this.btnMatricula.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.btnMatricula.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMatricula.UseVisualStyleBackColor = true;
+            // 
+            // PanelContenedorPrincipal
+            // 
+            this.PanelContenedorPrincipal.BackColor = System.Drawing.Color.Peru;
+            this.PanelContenedorPrincipal.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("PanelContenedorPrincipal.BackgroundImage")));
+            this.PanelContenedorPrincipal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PanelContenedorPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelContenedorPrincipal.Location = new System.Drawing.Point(170, 62);
+            this.PanelContenedorPrincipal.Name = "PanelContenedorPrincipal";
+            this.PanelContenedorPrincipal.Size = new System.Drawing.Size(799, 406);
+            this.PanelContenedorPrincipal.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 468);
+            this.Controls.Add(this.PanelContenedorPrincipal);
+            this.Controls.Add(this.panelMenu);
+            this.Controls.Add(this.PanelBarraTitulo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alquiler de Peliculas";
+            this.PanelBarraTitulo.ResumeLayout(false);
+            this.PanelBarraTitulo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelMenu.ResumeLayout(false);
+            this.panelMenu.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelBarraTitulo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnProfesores;
+        private System.Windows.Forms.Button btnStudents;
+        private System.Windows.Forms.Button btnMatricula;
+        private System.Windows.Forms.Panel PanelContenedorPrincipal;
+    }
+}
+
